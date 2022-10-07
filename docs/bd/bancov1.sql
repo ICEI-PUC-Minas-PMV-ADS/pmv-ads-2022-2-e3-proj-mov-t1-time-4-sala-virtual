183e4a9dbac80a6f1cc46ac9e3671735e6dee391
sabrinachaves <sabrinach08@gmail.com>
2022-10-07 08:51:40 -0300

    Adicionando ddl do banco v2

--- docs/bd/bancov1.sql
+++ docs/bd/bancov1.sql
@@ -11,7 +11,7 @@ create table Categories
 create table Documents
 (
     Id        int auto_increment
-        primary key,
+primary key,
     Type      int                                      not null,
     Number    varchar(14)                              not null,
     CreatedOn datetime(6) default CURRENT_TIMESTAMP(6) not null,
@@ -117,4 +117,79 @@ create table __EFMigrationsHistory
     ProductVersion varchar(32)  not null
 );
 
+create table Schedules
+(
+    Id         int auto_increment
+        primary key,
+    ClientId int                                        not null,
+    SpecialistId int                                    not null,
+    SpecialtyId int                                     not null,
+    PaymentId int                                       not null,
+    AppointmentId int,
+    StartDate  datetime(6)                              not null,
+    EndDate    datetime(6)                              not null,
+    CreatedOn  datetime(6) default CURRENT_TIMESTAMP(6) not null,
+    UpdatedOn  datetime(6) default CURRENT_TIMESTAMP(6) not null on update CURRENT_TIMESTAMP(6),
+    constraint FK_Schedules_Clients_ClientId
+        foreign key (ClientId) references Clients (Id)
+            on delete cascade
+);
+
+create table Appointments
+(
+    Id         int auto_increment
+        primary key,
+    ScheduleId int                                      not null,
+    CallStartedDate  datetime(6)                        not null,
+    CallEndedDate  datetime(6)                          not null,
+    ClientRating int,
+    SpecialistRating int,
+    CreatedOn  datetime(6) default CURRENT_TIMESTAMP(6) not null,
+    UpdatedOn  datetime(6) default CURRENT_TIMESTAMP(6) not null on update CURRENT_TIMESTAMP(6),
+    constraint FK_Appointments_Schedules_ScheduleId
+        foreign key (ScheduleId) references Schedules (Id)
+            on delete cascade
+);
+
+create table Payments
+(
+    Id         int auto_increment
+        primary key,
+    CreditId int                                        not null,
+    SpecialistAmount int                                not null,
+    PlatformFee int                                     not null,
+    Status int                                          not null,
+    CreatedOn  datetime(6) default CURRENT_TIMESTAMP(6) not null,
+    UpdatedOn  datetime(6) default CURRENT_TIMESTAMP(6) not null on update CURRENT_TIMESTAMP(6),
+    constraint FK_Payments_Credits_CreditId
+        foreign key (CreditId) references Credits (Id)
+            on delete cascade
+);
+
+create table Wallets
+(
+    Id         int auto_increment
+        primary key,
+    UserId int                                          not null,
+    Credits int                                         not null,
+    CreatedOn  datetime(6) default CURRENT_TIMESTAMP(6) not null,
+    UpdatedOn  datetime(6) default CURRENT_TIMESTAMP(6) not null on update CURRENT_TIMESTAMP(6),
+    constraint FK_Wallets_Users_UserId
+        foreign key (UserId) references Users (Id)
+            on delete cascade
+);
+
+create table Credits
+(
+    Id         int auto_increment
+        primary key,
+    WalletId int                                        not null,
+    Amount int                                          not null,
+    TransactionDate  datetime(6)                        not null,
+    CreatedOn  datetime(6) default CURRENT_TIMESTAMP(6) not null,
+    UpdatedOn  datetime(6) default CURRENT_TIMESTAMP(6) not null on update CURRENT_TIMESTAMP(6),
+    constraint FK_Credits_Wallets_WalletId
+        foreign key (WalletId) references Wallets (Id)
+            on delete cascade
+);
 
