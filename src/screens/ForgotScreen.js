727a680c424171b285313baaffd60edc6e38bef4
Mateus Melo <mateuscmelo@live.com>
2022-10-19 21:09:17 -0300

    Ajustes no layout.

--- src/screens/ForgotScreen.js
+++ src/screens/ForgotScreen.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import {Image, View, StyleSheet} from 'react-native';
+import {Image, StyleSheet} from 'react-native';
 import ScreenWrapper from '../components/ScreenWrapper';
 import ItemSeparator from '../components/ItemSeparator';
-import {sizing} from '../styles/sizing';
 import AppText from '../components/AppText';
 import InputLabel from '../components/InputLabel';
 import Button from '../components/Button';
@@ -12,48 +11,37 @@ import {useNavigation} from '@react-navigation/native';
 const ForgotScreen = () => {
   const navigation = useNavigation();
   return (
-    <ScreenWrapper style={styles.appContainer}>
-      <View style={{flex: 1}}>
-        <Image
-          style={styles.logoImage}
-          source={require('../assets/images/logo.png')}
-        />
-        <ItemSeparator />
-        <AppText color={'black'} align={'center'} size={16}>
-          {
-            'Para redefinir a sua senha, informe o usuário ou e-mail cadastrado na sua conta e lhe enviaremos um link com as instruções'
-          }
-        </AppText>
-        <ItemSeparator />
-        <InputLabel placeholder={'Email'} error />
-        <ItemSeparator />
-      </View>
+    <ScreenWrapper style={styles.container}>
+      <Image
+        style={styles.logoImage}
+        source={require('../assets/images/logo.png')}
+      />
+      <ItemSeparator />
+      <ItemSeparator size="xl" />
+      <AppText color={palette.black} align={'center'} size="m">
+        Para redefinir a sua senha, informe o usuário ou e-mail cadastrado na sua conta e lhe enviaremos um link com as instruções
+      </AppText>
+      <ItemSeparator size="xl" />
+      <InputLabel placeholder={'Email'} error />
+      <ItemSeparator size="xl" />
       <Button
         label="Enviar"
         color={palette.blue}
-        style={styles.ButtonStyle}
+        labelWeight="bold"
         labelColor={palette.white}
-        onPress={() => navigation.navigate('PasswordReset')}></Button>
+        onPress={() => navigation.navigate('PasswordReset')} />
     </ScreenWrapper>
   );
 };
 
 const styles = StyleSheet.create({
-  appContainer: {
-    alignItems: 'center',
+  container: {
     justifyContent: 'center',
-    marginHorizontal: 35,
   },
   logoImage: {
-    width: 320,
     height: 84,
     resizeMode: 'contain',
     alignSelf: 'center',
-    marginTop: 184,
-  },
-  ButtonStyle: {
-    marginTop: sizing.l,
-    width: '100%',
   },
 });
 
