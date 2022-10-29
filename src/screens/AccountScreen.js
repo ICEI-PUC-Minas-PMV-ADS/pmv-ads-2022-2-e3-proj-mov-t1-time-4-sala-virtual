33c6017a8ded72046f3294203d624f80144640a7
Mateus Melo <mateuscmelo@live.com>
2022-10-29 15:41:31 -0300

    Criação de rota e utilização de FlatList no menu.

--- src/screens/AccountScreen.js
+++ src/screens/AccountScreen.js
@@ -1,47 +1,90 @@
 import React from 'react';
 
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-
-import { StyleSheet, Text, View, Image } from 'react-native';
+import {StyleSheet, View, Alert, Image, FlatList} from 'react-native';
 import ScreenWrapper from '../components/ScreenWrapper';
 import Button from '../components/Button';
-import List from '../components/List';
 import { useAuth } from '../providers/auth';
-import { useTheme } from '@react-navigation/native';
 import { palette } from '../styles/palette';
-import ImageNameVertical from '../components/ImageNameVertical';
-import { sizing } from '../styles/sizing';
+import ItemSeparator from '../components/ItemSeparator';
+import AppText from '../components/AppText';
+import {sizing} from '../styles/sizing';
+import {fonts} from '../styles/fonts';
+import {accountScreens} from '../routes/account';
 
 const AccountScreen = () => {
-    
-    const { handleSignOut } = useAuth();
-    const { colors } = useTheme();
-    return (
-        <ScreenWrapper style={styles.appContainer}>
-            <ImageNameVertical></ImageNameVertical>
-            <List></List>
-            <View style={styles.btn}>
-                <Button label="Sair" labelColor={palette.blue} labelWeight="bold" labelSize="xl" muted onPress={handleSignOut} />
-            </View>
-            
-        </ScreenWrapper>
-    )
+  const SalaVirtualIcon = fonts.icons;
+  const signOutAlert = () =>
+    Alert.alert(
+      'Sair',
+      'Tem certeza de que deseja desconectar sua conta?',
+      [
+        {
+          text: "Não",
+          style: "cancel"
+        },
+        { text: "Sair", onPress: handleSignOut }
+      ]
+    );
+  const renderMenu = ({item}) => (
+    <View style={styles.rowContainer}>
+      <SalaVirtualIcon name={item.icon} size={sizing.l} />
+      <ItemSeparator size="l" />
+      <View style={styles.textContainer}>
+        <AppText size="s" weight="bold">{item.title}</AppText>
+        <AppText size="xs" weight="regular" color={palette.darkGray}>{item.description}</AppText>
+      </View>
+      <SalaVirtualIcon  name={'right-1'} size={sizing.l} />
+    </View>
+  );
+  const { handleSignOut, state } = useAuth();
+  const {user} = state;
+  return (
+      <ScreenWrapper>
+        <ItemSeparator size="xxl" />
+        <View style={styles.profileContainer}>
+          <Image style={styles.img} source={require('../assets/images/avatar.png')}></Image>
+          <ItemSeparator size="s" />
+          <AppText size="l" weight="bold"> {user.name}</AppText>
+        </View>
+        <ItemSeparator size="xl" />
+
+        <View style={styles.listContainer}>
+          <FlatList data={accountScreens} renderItem={renderMenu} keyExtractor={(item) => item.screen} ItemSeparatorComponent={() => <ItemSeparator size="s" />} />
+        </View>
+
+        <View style={styles.signOutContainer}>
+            <Button label="Sair" labelColor={palette.blue} labelWeight="bold" labelSize="xl" muted onPress={signOutAlert} />
+        </View>
+        <ItemSeparator size="l" />
+      </ScreenWrapper>
+  );
 };
 
 const styles = StyleSheet.create({
-    appContainer: {
-        flexDirection:'column',
-        alignItems: 'center'      
-    },
-    btn:{
-        flex: 1,
-      justifyContent:'flex-end',
-      paddingBottom: sizing['x']
-    }
-    
-    
-
-  
+  profileContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  signOutContainer: {
+    flex: 1,
+    justifyContent:'flex-end',
+  },
+  listContainer:{
+    flex: 1,
+  },
+  rowContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    paddingBottom: sizing.s,
+    borderBottomWidth: 1,
+    borderColor: palette.lightGray
+  },
+  textContainer: {
+    flex: 1,
+    flexDirection: 'column',
+  },
 });
 
 export default AccountScreen;
--- src/styles/sizing.js
+++ src/styles/sizing.js
@@ -2,8 +2,6 @@ export const sizing = {
   s: 8,
   m: 16,
   l: 24,
-  x: 32,
   xl: 48,
   xxl: 56,
-  xs: 64
 };
