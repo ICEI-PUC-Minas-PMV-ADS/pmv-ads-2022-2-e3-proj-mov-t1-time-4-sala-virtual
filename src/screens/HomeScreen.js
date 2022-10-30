fa83bb60937e863ec2e1d3f332b44802db8fb6f5
Mateus Melo <mateuscmelo@live.com>
2022-10-30 18:50:10 -0300

    Correção do banner na tela inicial do cliente e link para a tela de busca.

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {StyleSheet, View, Image} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
 import ScreenWrapper from '../components/ScreenWrapper';
 import AppText from '../components/AppText';
 
@@ -15,81 +16,79 @@ import {sizing} from '../styles/sizing';
 const SalaVirtualIcon = fonts.icons;
 
 const HomeScreen = () => {
-  const {state} = useAuth();
-  return (
-    <ScreenWrapper style={styles.appContainer} scroll>
-      <AppText
-        weight="bold"
-        size="l"
-        color={palette.darkBlue}
-        style={{marginTop: sizing.xl + sizing.m, alignSelf: 'flex-start'}}>
-        Olá, {state.user.name}!
-      </AppText>
-      <ItemSeparator size="l" />
-      <AppointmentInfo
-        title="Próxima Reunião"
-        specialist="Dra. Alice Costa"
-        specialty="Cardiologia"
-        date="10/10/2022"
-        weekDay="terça-feira"
-        startEndHour="09:00 AM - 10:00 AM"
-        duration="1 hora"
-        rounded></AppointmentInfo>
-
-      <ItemSeparator size="l" />
-
-      <Image
-        style={styles.bannerImage}
-        source={require('../assets/images/banner.jpg')}
-      />
-
-      <View style={{flexDirection: 'row'}}>
-        <Card
-          color={palette.lightBlue}
-          style={{
-            flexDirection: 'row',
-            borderBottomLeftRadius: sizing.s,
-            borderBottomRightRadius: sizing.s,
-          }}>
-          <View style={{flexDirection: 'row'}}>
-            <SalaVirtualIcon
-              name="search"
-              size={fonts.sizing.l + 1}
-              color={palette.white}
-              style={{alignSelf: 'center', paddingRight: sizing.m}}
+    const navigation = useNavigation();
+    const {state} = useAuth();
+    return (
+        <ScreenWrapper scroll>
+            <ItemSeparator size="l" />
+            <AppText weight="bold" size="l" color={palette.darkBlue}>Olá, {state.user.name}!</AppText>
+            <ItemSeparator size="l" />
+            <AppointmentInfo
+                title="Próxima Reunião"
+                specialist="Dra. Alice Costa"
+                specialty="Cardiologia"
+                date="10/10/2022"
+                weekDay="terça-feira"
+                startEndHour="09:00 AM - 10:00 AM"
+                duration="1 hora"
+                rounded
             />
-          </View>
-          <View>
-            <AppText
-              color={palette.white}
-              weight="bold"
-              size={fonts.sizing.m + 2}>
-              Precisa de orientação?
-            </AppText>
-            <AppText color={palette.white} size={fonts.sizing.m + 2}>
-              Encontre um especialista
-            </AppText>
-          </View>
-        </Card>
-      </View>
-
-      <ItemSeparator size="l"/>
-    </ScreenWrapper>
-  );
+            <ItemSeparator size="l" />
+            <Card
+                onPress={() => navigation.navigate('Search')}
+                style={styles.bannerContainer}
+                color={palette.lightBlue}
+                rounded>
+                <Image
+                    style={styles.bannerImage}
+                    source={require('../assets/images/banner.jpg')}
+                />
+                <View style={styles.infoContainer}>
+                    <View style={styles.iconContainer}>
+                        <SalaVirtualIcon
+                            name="search"
+                            size={fonts.sizing.l + 2}
+                            color={palette.white}
+                        />
+                    </View>
+                    <ItemSeparator size="m" />
+                    <View style={styles.textContainer}>
+                        <AppText color={palette.white} weight="bold" size={fonts.sizing.m + 2}>
+                            Precisa de orientação?
+                        </AppText>
+                        <AppText color={palette.white} size={fonts.sizing.m + 2}>
+                          Encontre um especialista
+                        </AppText>
+                    </View>
+                </View>
+            </Card>
+        </ScreenWrapper>
+    );
 };
 
 const styles = StyleSheet.create({
-  appContainer: {
-    paddingHorizontal: sizing.l + sizing.s,
-  },
-  bannerImage: {
-    height: 250,
-    width: '100%',
-    resizeMode: 'contain',
-    alignSelf: 'center',
-    borderTopLeftRadius: sizing.s,
-    borderTopRightRadius: sizing.s,
-  },
+    bannerContainer: {
+        padding: 0,
+    },
+    bannerImage: {
+        height: 250,
+        width: '100%',
+        resizeMode: 'cover',
+        alignSelf: 'center',
+        borderTopLeftRadius: sizing.s,
+        borderTopRightRadius: sizing.s,
+    },
+    infoContainer: {
+        flex: 1,
+        flexDirection: 'row',
+        padding: sizing.m,
+    },
+    iconContainer: {
+        justifyContent: 'center',
+    },
+    textContainer: {
+        flex: 1,
+    }
 });
 
 export default HomeScreen;
