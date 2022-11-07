a20ae61c924fa10ba27eb5f6f7a40b05db4c8d4c
Mateus Melo <mateuscmelo@live.com>
2022-11-07 08:28:51 -0300

    Conexão da tela de agendamento com tela de especialistas.

---
+++ src/screens/ScheduleScreen.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ScreenWrapper from '../components/ScreenWrapper';
+
+const ScheduleScreen = () => {
+    return (
+        <ScreenWrapper>
+        </ScreenWrapper>
+    );
+};
+
+export default ScheduleScreen;
